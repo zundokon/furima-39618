6c05bff9686a4deefa56a5dbea29cc38cbea890d
zundokon <tete5337@yahoo.co.jp>
2023-08-17 18:05:55 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,19 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,   presence: true
-  validates :first_name, presence: true
-  validates :last_name,  presence: true
-  validates :kana_first, presence: true
-  validates :kana_last,  presence: true
   validates :birthdate,  presence: true
 
-  
+  with_options presence: true,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Input full-width characters' } do
+    validates :first_name
+    validates :last_name
+
+  end
+  with_options presence: true,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Input full-width katakana characters' } do
+    validates :kana_first
+    validates :kana_last
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers' 
+
 end
