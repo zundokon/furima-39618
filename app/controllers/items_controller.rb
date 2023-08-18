177273b6a8a28a2b8e096dcfae552bb65f293700
zundokon <tete5337@yahoo.co.jp>
2023-08-18 16:06:45 +0900

    コメントアウト修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
-  <%# 詳細機能 %>
-  def show
-  end
-  <%# //詳細機能 %>
-  
+#  詳細機能
+#   def show
+#   end
+#  詳細機能
+
 end
