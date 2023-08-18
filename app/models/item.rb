d9c80ef027b58edf23ab4c88869d611459ce8977
zundokon <tete5337@yahoo.co.jp>
2023-08-18 21:38:59 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,13 @@ class Item < ApplicationRecord
   belongs_to :prefecture, :category, :condition, :shipping_fee, :shipping_duration
   
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  validates :name,                   presence: true
+  validates :description,            presence: true
+  validates :category_id,            presence: true
+  validates :condition_id,           presence: true
+  validates :prefecture_id,          presence: true
+  validates :shipping_fee_id,        presence: true
+  validates :shipping_duration_id,   presence: true
+  validates :price,                  presence: true
 end
