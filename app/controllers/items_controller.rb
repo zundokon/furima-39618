9370f86f09f0612f0d0276d5b6d66d9300fdb8fc
zundokon <tete5337@yahoo.co.jp>
2023-08-23 20:47:05 +0900

    削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update, ]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def show
     @user = @item.user
@@ -39,7 +39,16 @@ class ItemsController < ApplicationController
     render:edit, status: :unprocessable_entity
     end
   end
-  
+
+  def destroy
+    if @item.user_id == current_user.id
+      @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
   private
 
   def item_params
