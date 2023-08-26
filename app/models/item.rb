ce70c9bb36251af570a50254771cf2e094435fdf
zundokon <tete5337@yahoo.co.jp>
2023-08-26 12:32:24 +0900

    アソシエーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  has_many :orders
+  has_one :orders
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
