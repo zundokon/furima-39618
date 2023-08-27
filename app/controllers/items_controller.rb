4674d606163abd248df0c32c90c743fe1e3e8557
zundokon <tete5337@yahoo.co.jp>
2023-08-27 21:26:24 +0900

    編集機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,7 +25,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if @item.user_id == current_user.id && @item.orders.nil?
+    if @item.user_id == current_user.id && @item.order.nil?
     else
       redirect_to root_path
     end
